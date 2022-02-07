9e6078d8e21d60ae022015e5188ee536790939d2
mitikusayuu <76817409+mitikusayuu@users.noreply.github.com>
2022-02-08 04:37:39 +0900

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,10 @@
-def main():
-  print('You rolled a die')
+import random as ra
 
+def main():
+  roll = ra.randint(1,6)
+  print(f'You rolled a {roll}')
+  
+  
+  
 if __name__== "__main__":
   main()
